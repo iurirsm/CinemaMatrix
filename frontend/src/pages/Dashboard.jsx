19d925c8e7ffc8b6f54ae8f5369eb49246a187c1
stphalstaro <sfiguei5@my.centennialcollege.ca>
2024-11-22 15:11:50 -0500

    Removed logout button. Included contents of MovieForm and MovieList into the Dashboard page and made sure the link corropsonded correctly

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,15 +4,12 @@ import MovieList from '../components/MovieList';
 import MovieForm from '../components/MovieForm';
 
 const Dashboard = () => {
-  const { user, logout } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-between align-items-center">
         <h2>Welcome, {user.username}</h2>
-        <button className="btn btn-secondary" onClick={logout}>
-          Logout
-        </button>
       </div>
       <MovieForm />
       <MovieList />
@@ -21,3 +18,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
